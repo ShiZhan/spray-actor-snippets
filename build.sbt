4b5ea209cfa4e26b2ff09dbf3c64b1c7cae4d46e
ShiZhan <g.shizhan.g@gmail.com>
2014-07-08 17:50:18 -0500

    framework test

--- build.sbt
+++ build.sbt
@@ -4,8 +4,10 @@ version := "1.0"
 
 scalaVersion := Option(System.getProperty("scala.version")).getOrElse("2.10.4")
 
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+ 
 libraryDependencies ++= Seq(
-  "org.scala-lang" % "scala-actors" % "2.10.4",
+  "com.typesafe.akka" % "akka-actor_2.10" % "2.3.4",
   "org.apache.jena" % "jena-core" % "2.11.1" excludeAll(ExclusionRule(organization = "org.slf4j")), 
   "org.apache.jena" % "jena-arq" % "2.11.1" excludeAll(ExclusionRule(organization = "org.slf4j")), 
   "org.apache.jena" % "jena-tdb" % "1.0.1" excludeAll(ExclusionRule(organization = "org.slf4j")),
---
+++ src/test/scala/AkkaTest.scala
@@ -0,0 +1,22 @@
+import akka.actor.{ Actor, ActorRef, ActorSystem, Props }
+
+class Controller extends Actor {
+  def receive = {
+    case "hello" => println("hello back at you")
+    case "exit" => exit
+    case _ => println("huh?")
+  }
+}
+
+object AkkaTest extends App {
+  def console(controller: ActorRef): Unit =
+    for (line <- io.Source.stdin.getLines) line.split(' ').toList match {
+      case "exit" :: Nil =>
+        controller ! "exit"; return
+      case any => any.foreach(controller ! _)
+    }
+
+  val system = ActorSystem("CoreSystem")
+  val controller = system.actorOf(Props[Controller], name = "controller")
+  console(controller)
+}
