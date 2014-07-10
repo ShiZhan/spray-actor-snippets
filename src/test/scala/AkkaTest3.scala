fe16eddb3dfa57b63a4ab7a9d37a906588f2d94f
ShiZhan <g.shizhan.g@gmail.com>
2014-07-10 18:00:12 -0500

    Futures

--- src/test/scala/AkkaTest3.scala
+++ src/test/scala/AkkaTest3.scala
@@ -4,19 +4,16 @@ object AkkaTest3 {
   import akka.util.Timeout
   import scala.concurrent.{ Await, Future }
   import scala.concurrent.duration._
-
-  val system = ActorSystem("CoreSystem")
+  import scala.concurrent.ExecutionContext.Implicits.global
 
   class Leader(followers: Array[ActorRef]) extends Actor {
     implicit val timeout = Timeout(5 seconds)
     def receive = {
       case "hello" => println("hello back at you")
       case "report" =>
-        val result = followers
-          .map(_ ? "report")
-          .map(_.mapTo[String])
-          .map(Await.result(_, timeout.duration))
-        println(result.mkString("---\n", "\n", "\n---"))
+        val futures = followers.map(_ ? "report").map(_.mapTo[String])
+        val results = Future.reduce(futures)(_ + "\n---\n" + _)
+        results.foreach(println)
       case "exit" => { followers.foreach(_ ! "exit"); exit }
       case _ => println("huh?")
     }
@@ -24,7 +21,7 @@ object AkkaTest3 {
 
   class Follower(index: Int) extends Actor {
     def receive = {
-      case "report" => sender ! (index + ": stand ready")
+      case "report" => { Thread.sleep((1 seconds).toMillis); sender ! (index + ": stand ready") }
       case "exit" => { println(index + ": stand down"); exit }
       case _ => println("huh?")
     }
@@ -37,6 +34,8 @@ object AkkaTest3 {
     }
 
   def main(args: Array[String]) = {
+    val system = ActorSystem("CoreSystem")
+
     val followers = Array.tabulate(args.head.toInt) { index =>
       system.actorOf(Props(new Follower(index)), name = "follower" + "%03d".format(index))
     }
