2c569cb6a546c9c51e53ff527e21089c01e3ae80
ShiZhan <g.shizhan.g@gmail.com>
2014-07-15 16:56:52 -0500

    parameters

--- src/main/scala/LiveTriples.scala
+++ src/main/scala/LiveTriples.scala
@@ -1,16 +1,18 @@
 object LiveTriples {
-  import akka.actor.{ActorSystem, Props}
+  import akka.actor.{ ActorSystem, Props }
   import akka.io.IO
   import spray.can.Http
   import akka.pattern.ask
   import akka.util.Timeout
   import scala.concurrent.duration._
 
-  def main(args: Array[String]) = {
-    implicit val system = ActorSystem("CoreSystem")
-    val service = system.actorOf(Props[LiveTriplesServiceActor], "live-triples-service")
-    implicit val timeout = Timeout(5.seconds)
-    IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
-    println("Service started.")
+  def main(args: Array[String]) = args.toList match {
+    case host :: port :: Nil if (port.forall(_.isDigit)) =>
+      implicit val system = ActorSystem("CoreSystem")
+      val service = system.actorOf(Props[LiveTriplesServiceActor], "live-triples-service")
+      implicit val timeout = Timeout(5.seconds)
+      IO(Http) ? Http.Bind(service, interface = host, port = port.toInt)
+      println("Service started.")
+    case _ => println("LiveTriples <host> <port>")
   }
 }
